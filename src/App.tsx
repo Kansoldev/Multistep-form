b287d037c44830c8c58548799d54e299f741d855
Oyinkansola Yahaya <yahayaoyinkansola@gmail.com>
2025-02-25 16:07:23 +0100

    Add second step of form

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,33 @@ import "./App.css";
 
 function App() {
   const [currentStep, setCurrentStep] = useState(1);
+  const [radioChecked, setRadioChecked] = useState(false);
   const [inputs, setInputs] = useState({
     name: "",
     email: "",
     phone: "",
   });
+
+  const plans = [
+    {
+      name: "arcade",
+      icon: "/icon-arcade.svg",
+      monthPrice: "$9/mo",
+      yearPrice: "$90/yr",
+    },
+    {
+      name: "advanced",
+      icon: "/icon-advanced.svg",
+      monthPrice: "$12/mo",
+      yearPrice: "$120/yr",
+    },
+    {
+      name: "pro",
+      icon: "/icon-pro.svg",
+      monthPrice: "$15/mo",
+      yearPrice: "$150/yr",
+    },
+  ];
 
   function updateFields(event: ChangeEvent<HTMLInputElement>) {
     setInputs((prevInputs) => {
@@ -80,9 +102,101 @@ function App() {
             <button
               type="button"
               className="block bg-[#02295a] text-white px-6 py-3 mt-10 md:mt-28 ml-auto rounded-lg"
+              onClick={() => setCurrentStep(2)}
             >
               Next Step
             </button>
+          </div>
+        )}
+
+        {currentStep === 2 && (
+          <div className="bg-white md:bg-transparent rounded-xl -mt-10 mx-5 md:mt-10 md:mx-0 shadow-xl md:shadow-none p-7 md:p-0">
+            <h2 className="font-bold text-3xl text-[#02295a]">
+              Select your plan
+            </h2>
+
+            <p className="mt-2 md:mb-7 text-[#9699ab]">
+              You have the option of monthly/yearly billing
+            </p>
+
+            <div className="flex flex-col md:flex-row justify-between gap-5 text-white mt-5">
+              {plans.map((plan) => (
+                <label key={plan.name} htmlFor={plan.name} className="flex-1">
+                  <input
+                    type="radio"
+                    name="options"
+                    id={plan.name}
+                    value={plan.name}
+                    className="absolute opacity-0 w-0 h-0"
+                    defaultChecked={plan.name === "arcade"}
+                  />
+
+                  <div className="flex items-center gap-3 border border-solid border-[#166a79] md:inline-block p-3 pr-14 rounded-md w-full relative">
+                    <div className="w-10 h-10 rounded-full">
+                      <img src={plan.icon} alt="" />
+                    </div>
+
+                    <div className="plan-details">
+                      <h4 className="text-[#02295a] font-bold md:mt-10">
+                        {/* Make first letter capital */}
+                        {plan.name.substring(0, 1).toUpperCase() +
+                          plan.name.substring(1)}
+                      </h4>
+
+                      <p className="text-[#9699ab]">
+                        {radioChecked ? plan.yearPrice : plan.monthPrice}
+                      </p>
+
+                      {radioChecked && (
+                        <span className="mt-2 text-[#02295a]">
+                          2 months free
+                        </span>
+                      )}
+                    </div>
+                  </div>
+                </label>
+              ))}
+            </div>
+
+            <div className="bg-[#f8f9fe] mt-10 flex justify-center items-center gap-6 py-3 rounded-md">
+              <span
+                className={`text-[#02295a] text-sm ${
+                  !radioChecked ? "font-bold" : ""
+                }`}
+              >
+                Monthly
+              </span>
+
+              <label className="switch">
+                <input
+                  type="checkbox"
+                  onChange={() => setRadioChecked(!radioChecked)}
+                />
+                <span className="slider"></span>
+              </label>
+
+              <span
+                className={`text-[#02295a] text-sm ${
+                  radioChecked ? "font-bold" : ""
+                }`}
+              >
+                Yearly
+              </span>
+            </div>
+
+            <div className="flex justify-between w-full">
+              <button
+                type="button"
+                className="px-6 py-3 mt-10 md:mt-24 block text-[#02295a] rounded-lg"
+                onClick={() => setCurrentStep((prevStep) => prevStep - 1)}
+              >
+                Go Back
+              </button>
+
+              <button className="px-6 py-3 mt-10 md:mt-24 block bg-[#02295a] text-white rounded-lg">
+                Next Step
+              </button>
+            </div>
           </div>
         )}
       </form>
